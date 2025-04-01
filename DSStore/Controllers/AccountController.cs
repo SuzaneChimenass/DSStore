14047e4b6776724be570548a5437fa1b6d3b9fb2
SuzaneChimenass <suzanecamila879@gmail.com>
2025-03-31 22:39:51 -0300

    Programando código Post do Login e controller

--- DSStore/Controllers/AccountController.cs
+++ DSStore/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using DSStore.ViewModels;
 using NuGet.ProjectModel;
+using System.Net.Mail;
+using System.Security.Claims;
 
 namespace DSStore.Controllers;
 
@@ -34,6 +36,58 @@ public class AccountController : Controller
         {
             UrlRetorno = returnUrl ?? Url.Content("~/")
         };
+        return View(login);
+    }
+
+    public bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress m = new(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
+
+            var result = await _singInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded) {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirected(login.UrlRetorno);
+            }
+
+            if (result.IsLockedOut) {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                ModelState.AddModelError("", "Sua conta está bloqueada, aguarde alguns minutos e tente novamente!!!");
+            }
+            else
+            if (result.IsNotAllowed) {
+                _logger.LogWarning($"Usuário {login.Email} não confirmou sua conta");
+                ModelState.AddModelError(string.Empty, "Sua conta não está confirmada, verifique seu email!!!");
+            }
+
+            else
+                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!!");
+        }
         return View(login);
     }
 }
