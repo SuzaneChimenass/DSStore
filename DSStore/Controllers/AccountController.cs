2dc52dbdbf977840a5b5a99172f34109080e8ef4
SuzaneChimenass <suzanecamila879@gmail.com>
2025-04-04 22:37:50 -0300

    Adicionando página de registro

--- DSStore/Controllers/AccountController.cs
+++ DSStore/Controllers/AccountController.cs
@@ -86,6 +86,13 @@ public class AccountController : Controller
         return RedirectToAction("Index", "Home");
     }
 
+    [HttpGet]
+    public IActionResult Registro()
+    {
+        RegistroVM register = new();
+        return View(register);
+    }
+
     public bool IsValidEmail(string email)
     {
         try
